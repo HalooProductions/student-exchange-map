2eb11cccfe410fd01051b6b10cb0faf2c7fcaf7e
Riku K Wikman <Riku.Wikman@edu.savonia.fi>
2016-09-23 10:24:59 +0300

    Siivottu main.js

--- js/main.js
+++ js/main.js
@@ -32,35 +32,38 @@ var styles = [
   }
 ];
 
+var mapOptions = {
+  center: {lat: 30, lng: 0},
+  scrollwheel: true,
+  zoom: 3,
+  disableDefaultUI: true,
+  styles: styles
+};
+
 var map;
 
 function initMap () {
   // Create a map object and specify the DOM element for display.
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 30, lng: 0},
-    scrollwheel: true,
-    zoom: 3,
-    disableDefaultUI: true,
-    styles: styles
-  });
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
 
 function focusCountry (index) {
   $.getJSON('data/countries.json', function (countries) {
-           var myOptions = {
-               center: new google.maps.LatLng(
-                   countries[index].center_lat,
-                   countries[index].center_lng),
-           }
-           var map = new google.maps.Map(document.getElementById("map"), myOptions);
+    var myOptions = {
+      center: new google.maps.LatLng(
+        countries[index].center_lat,
+        countries[index].center_lng),
+      }
 
-           // set the bounds of the map
-           var bounds = new google.maps.LatLngBounds(
-               new google.maps.LatLng(countries[index].sw_lat, countries[index].sw_lng),
-               new google.maps.LatLng(countries[index].ne_lat, countries[index].ne_lng) );
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-           map.fitBounds(bounds);
-       });
+    var bounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(countries[index].sw_lat, countries[index].sw_lng),
+      new google.maps.LatLng(countries[index].ne_lat, countries[index].ne_lng)
+    );
+
+    map.fitBounds(bounds);
+  });
 }
 
 function init () {
