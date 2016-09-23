62b537f55111ed9f3d69903f15f35b20ef1fd34c
Riku Wikman <riku.wikman@gmail.com>
2016-09-23 10:05:36 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,81 @@
+var settings = {
+  viewHeight: $(window).height(),
+  highlighted: 'none'
+};
 
+var styles = [
+  {
+    featureType: "administrative.country",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "administrative.province",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  { 
+    featureType: "road", 
+    stylers: [
+      { 
+        visibility: "off" 
+      } 
+    ] 
+  }
+];
+
+var map;
+
+function initMap () {
+  // Create a map object and specify the DOM element for display.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 30, lng: 0},
+    scrollwheel: true,
+    zoom: 3,
+    disableDefaultUI: true,
+    styles: styles
+  });
+}
+
+function focusCountry (index) {
+  $.getJSON('data/countries.json', function (countries) {
+           var myOptions = {
+               center: new google.maps.LatLng(
+                   countries[index].center_lat,
+                   countries[index].center_lng),
+           }
+           var map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+           // set the bounds of the map
+           var bounds = new google.maps.LatLngBounds(
+               new google.maps.LatLng(countries[index].sw_lat, countries[index].sw_lng),
+               new google.maps.LatLng(countries[index].ne_lat, countries[index].ne_lng) );
+
+           map.fitBounds(bounds);
+       });
+}
+
+function init () {
+  map = $("#map");
+  map.height(settings.viewHeight);
+  initMap();
+}
+
+$(document).ready(function () {
+  init();
+  $('.ui.dropdown').dropdown();
+
+  $('.ui.dropdown').on('change', function (evt) {
+    if (!isNaN(evt.target.value)) {
+      focusCountry(parseInt(evt.target.value));
+    }
+  });
+});
