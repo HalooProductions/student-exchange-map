52ad592cf59c31b79ef9591ec495ca4a5fa45b9c
Riku K Wikman <Riku.Wikman@edu.savonia.fi>
2016-11-01 14:52:43 +0200

    Testi lisätty

---
+++ tests/SchoolTest.php
@@ -0,0 +1,34 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+include_once('School.php');
+include_once('DB.php');
+
+$conn = new DB;
+$conn->connect();
+
+
+class SchoolTest extends TestCase
+{
+    // ...
+
+    public function testCanBeNegated()
+    {
+        // Arrange
+        $a = new School($conn);
+
+        // Act
+        $a->create([
+            'name' => 'Savonia Ammattikorkeakoulu',
+            'country' => 1,
+            'city' => 2,
+            'place_id' => 'ChIJUYf0dHe6hEYRKaYg4vlkF28',
+            'departments' => [1, 3],
+        ])->save();
+
+        $b = new School($conn);
+
+        // Assert
+        $this->assertEquals('ChIJUYf0dHe6hEYRKaYg4vlkF28', $b->where(['place_id' => 'ChIJUYf0dHe6hEYRKaYg4vlkF28'])->first()->name);
+    }
+}
